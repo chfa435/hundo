2313ec170a0fd2864bbf0fee2ee95cb6a1627f66
chfa435 <79060477+chfa435@users.noreply.github.com>
2022-04-11 11:12:59 -0400

    add description of Hundo

--- js/site.js
+++ js/site.js
@@ -1,27 +1,38 @@
- 
  //Step ONE (GET = controller, accepts a request)
-function getMessage(){
+ function getValues() {
+     let startValue = document.getElementById("startValue").value;
+     let endValue = document.getElementById("endValue").value;
 
-    let msg = document.getElementById("txtMessage").value;
+     displayValues(startValue, endValue);
+ }
 
-    displayMessage(msg);
+ //Final Step (DISPLAY = view)
+ function displayValues(start, end) {
 
-}
+     //first get the ol element from the page
+     element = document.getElementById("results");
 
-//Final Step (DISPLAY = view)
-function displayMessage(message) {
-     
-    //first get the ol element from the page
-    element = document.getElementById("results");
-    
-    //next create a new li element
-    let item = document.createElement("li");
-    //add classes to the li element
-    item.classList.add("list-group-item");
-    //set the message for the li element
-    item.innerHTML = message;
+     for (let i = start; i <= end; i++) {
 
-    //add the new item to the list
-    element.appendChild(item);
-}
+         //next create a new li element
+         let item = document.createElement("div");
 
+         if (i % 2 == 0) {
+             item.classList.add("boldItem");
+         }
+
+         else {
+            item.classList.add("Item");
+        }
+
+         //add classes to the li element
+         item.classList.add("col");
+
+         //set the message for the li element
+         item.innerHTML = i;
+
+         //add the new item to the list
+         element.appendChild(item);
+
+     }
+ }
